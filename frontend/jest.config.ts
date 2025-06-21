c0b86d525ae319842e1ebbdd4f2a7a188564d170
emiliodeleonhdez <emiliodeleon.contact@gmail.com>
2025-06-20 20:14:32 -0600

    Config(FE):  Added transofrm and module name mapper to jest condig

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -3,12 +3,19 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   clearMocks: true,
   collectCoverage: true,
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
 };
 
 export default config;
