caa83e74f9c05aa9126de8d4865f5c99dd84d5cf
emiliodeleonhdez <emiliodeleon.contact@gmail.com>
2025-06-22 16:23:37 -0600

    Fix(BE): Allow trafic from FrontEnd using env var

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get(`${apiVersion}/health`, (_, res) => {
