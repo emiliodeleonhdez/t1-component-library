f1501e3634ce09b7f672da6cb75162c1b077959e
emiliodeleonhdez <emiliodeleon.contact@gmail.com>
2025-06-21 23:44:08 -0600

    Fix(Layout): added h screen and overflow to parent container of the main tag

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       >
         <div className="flex min-h-screen">
           <SideBar />
-          <div className="flex flex-col flex-1">
+          <div className="flex flex-col flex-1 h-screen overflow-auto">
             <Header />
             <main className="flex-1 container mx-auto px-4 py-6">
               {children}
